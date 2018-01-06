e426633e0d7cd01c86327a0100b6e7e4c4def828
David Joy <david.joy@nist.gov>
2018-01-05 19:54:29 -0500

    Added SparseSignal and test cases

---
+++ lib/sparse_signal.py
@@ -0,0 +1,48 @@
+from operator import add, sub
+
+class SparseSignal(dict):
+    def __init__(self, *args, **kwargs):
+        super(SparseSignal, self).__init__(*args, **kwargs)
+
+    def join(self, other, join_func, default = 0):
+        out_signal = SparseSignal()
+
+        self_val, other_val = default, default
+        last_val = default
+
+        for key in sorted(set(self.keys()) | set(other.keys())):
+            self_val = self.get(key, self_val)
+            other_val = other.get(key, other_val)
+            
+            new_val = join_func(self_val, other_val)
+            if new_val != last_val:
+                out_signal[key] = new_val
+                last_val = new_val
+
+        return out_signal
+
+    def __add__(self, other):
+        return self.join(other, add)
+            
+    def __and__(self, other):
+        return self.join(other, min)
+
+    def __or__(self, other):
+        return self.join(other, max)
+
+    def __sub__(self, other):
+        return self.join(self & other, sub)
+
+    def normalize(self):
+        return self + {}
+
+    def area(self):
+        last_t, last_v = None, None
+        area = 0
+
+        for t in sorted(self.keys()):
+            if last_t != None and last_v != None:
+                area += (t - last_t) * last_v
+            last_t, last_v = t, self[t]
+
+        return area
---
+++ test/test_sparse_signal.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+
+import sys
+import os
+
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../lib")
+sys.path.append(lib_path)
+
+import unittest
+from operator import add
+from sparse_signal import SparseSignal as S
+
+class TestSparseSignal(unittest.TestCase):
+    def setUp(self):
+        super(TestSparseSignal, self).setUp()
+
+        self.se = S({})
+        self.s1 = S({30: 1, 60: 0})
+        self.s2 = S({0: 1, 10: 0})
+        self.s3 = S({30: 1, 40: 0})
+        self.s4 = S({60: 1, 80: 0})
+        self.s5 = S({0: 1, 30: 0})
+        self.s6 = S({60: 1, 100: 0})
+        self.s7 = S({0: 1, 100: 0})
+        
+        self.sb1 = S({0: 1, 30: 2, 60: 1, 100: 0})
+        self.sb2 = S({30: 2, 40: 1, 60: 0})
+        self.sb3 = S({30: 1, 60: 2, 100: 1, 130: 0})
+
+        self.ss1 = S({0: 1, 30: 0, 60: 1, 100: 0})
+
+        self.si1 = S({45: 1, 100: 0})
+
+        self.sd1 = S({10: 1, 20: 0})
+        self.sd2 = S({10.45: 1, 20: 0})
+        self.sd3 = S({10.00: 1, 20.85: 0})
+        self.sd4 = S({10.90: 1, 20.35: 0})
+        self.sd5 = S({0.12: 1, 30.32: 0, 30.7: 1, 100.2: 0})
+        self.sd6 = S({0.12: 1, 30.32: 2, 60.7: 1, 100.2: 0})
+
+        self.sn1 = S({0: 1, 10: 1, 20: 0})
+
+    def testSignalEquivalence(self):
+        self.assertEqual(self.se, self.se)
+        self.assertEqual(self.s1, self.s1)
+        self.assertEqual(S({30: 1, 60: 0}), self.s1)
+
+    def testSignalAdditionEmpty(self):
+        self.assertEqual(self.se + self.se, self.se)
+        self.assertEqual(self.s1 + self.se, self.s1)
+        self.assertEqual(self.se + self.s1, self.s1)
+
+    def testSignalAdditionPass2(self):
+        self.assertEqual(self.s1 + self.s2, S({0: 1, 10: 0, 30: 1, 60: 0}))
+        self.assertEqual(self.s1 + self.s3, S({30: 2, 40: 1, 60: 0}))
+        self.assertEqual(self.s1 + self.s4, S({30: 1, 80: 0}))
+
+    def testSignalAdditionPass3(self):
+        self.assertEqual(self.s1 + self.s5, S({0: 1, 60: 0}))
+        self.assertEqual(self.s1 + self.s1, S({30: 2, 60: 0}))
+        self.assertEqual(self.s1 + self.s6, S({30: 1, 100: 0}))
+        self.assertEqual(self.s1 + self.s7, S({0: 1, 30: 2, 60: 1, 100: 0}))
+
+    def testSignalMultiAdd(self):
+        self.assertEqual(reduce(add, [self.s1, self.s1, self.s1]), S({30: 3, 60: 0}))
+        self.assertEqual(reduce(add, [self.s1, self.s2, self.s3]), S({0: 1, 10: 0, 30: 2, 40: 1, 60: 0}))
+
+    def testIntersection(self):
+        self.assertEqual(self.s1 & self.se, self.se)
+        self.assertEqual(self.s1 & self.s1, self.s1)
+        self.assertEqual(self.s1 & self.s2, self.se)
+        self.assertEqual(self.s1 & self.s3, self.s3)
+        self.assertEqual(self.s3 & self.s1, self.s3)
+        self.assertEqual(self.s1 & self.sb1, self.s1)
+        self.assertEqual(self.s1 & self.si1, S({45: 1, 60: 0}))
+
+    def testSubtraction(self):
+        self.assertEqual(self.s1 - self.se, self.s1)
+        self.assertEqual(self.se - self.s1, self.se)
+        self.assertEqual(self.s1 - self.si1, S({30: 1, 45: 0}))
+        self.assertEqual(self.si1 - self.s1, S({60: 1, 100: 0}))
+        self.assertEqual(self.sb1 - self.s1, S({0: 1, 100: 0}))
+        self.assertEqual(self.sb1 - self.sb3, S({0: 1, 60: 0}))
+        self.assertEqual(self.sb3 - self.sb1, S({60: 1, 130: 0}))
+        self.assertEqual(self.sb1 - self.se, self.sb1)
+
+    def testArea(self):
+        self.assertEqual(self.se.area(), 0)
+        self.assertEqual(self.s1.area(), 30)
+        self.assertEqual(self.s2.area(), 10)
+        self.assertEqual(self.sb1.area(), 130)
+
+    def testNormalize(self):
+        self.assertEqual(self.sn1.normalize(), S({0: 1, 20: 0}))
+        
+if __name__ == '__main__':
+    unittest.main()
