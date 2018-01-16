bf33607a27d5790f919d72bf0a7f90b2707f78d3
David Joy <david.joy@nist.gov>
2018-01-16 12:55:50 -0500

    Updated ytick labels for DET plot

--- lib/plot.py
+++ lib/plot.py
@@ -7,19 +7,15 @@ from scipy.stats import norm
 import pprint
 
 def det_single_curve(points, out_fn):
-    ytick_values = [.0001, 0.0002, 0.0005, 0.001, 0.002,
-                    0.005, .01, .02, .05, .10, .20, .40,
-                    .60, .80, .90, .95, .98, .99, .995, .999]
+    ytick_values = [0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.98, 0.99, 0.995, 0.999]
     ytick = norm.ppf(ytick_values)
-    y_tick_labels = ['0.01', '0.02', '0.05', '0.1', '0.2',
-                     '0.5', '1', '2', '5', '10', '20', '40',
-                     '60', '80', '90', '95', '98', '99', '99.5', '99.9']
+
     plt.plot([ x for ds, x, y in points ], [ norm.ppf(y) for ds, x, y in points ])
     
     plt.xscale("log")
     plt.xticks([0.01, 0.1, 1, 10], ["0.01", "0.1", "1", "10"])
     
-    plt.yticks(ytick, y_tick_labels)
+    plt.yticks(ytick, [ str(y * 100) for y in ytick_values ])
 
     plt.savefig(out_fn)
     plt.close()
