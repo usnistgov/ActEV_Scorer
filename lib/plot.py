24b7b5a898d4926b5ecdc2d68350308f814c9a60
David Joy <david.joy@nist.gov>
2018-03-23 12:38:59 -0400

    Fixed issue with missing DET curve points

--- lib/plot.py
+++ lib/plot.py
@@ -36,26 +36,45 @@ import matplotlib.pyplot as plt
 
 from scipy.stats import norm
 
+def _make_clamp(_min, _max):
+    def clamp(n):
+        if n < _min:
+            return _min
+        elif n > _max:
+            return _max
+        else:
+            return n
+
+    return clamp
+
 def det_curve(points_dict, out_fn):
     ytick_values = [0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.98, 0.99, 0.995, 0.999]
+    ytick_labels = [ str(y * 100) for y in ytick_values ]
     ytick = norm.ppf(ytick_values)
+
+    # norm.ppf for 0 and 1 is -inf and inf respectively, so we clamp
+    # those values to something reasonably smaller/larger than the
+    # min/max yticks so that matplotlib can plot them, but they won't
+    # be shown in the plot view
+    clamp = _make_clamp(ytick[0] - 10, ytick[-1] + 10)
     
     xtick_values = [0.01, 0.1, 1, 10]
 
     plt.figure(num=0, figsize=(7, 6.5), dpi=240)
     for k, points in points_dict.iteritems():
-        plt.plot([ x for ds, x, y in points ], [ norm.ppf(y) for ds, x, y in points ], label = k)
+        plt.plot([ x for ds, x, y in points ], [ clamp(norm.ppf(y)) for ds, x, y in points ], label = k)
 
     # Need the following line for matplotlib 1.5.1.  Without explicit
     # limits, matplotlib attempts to infer them from the data, but if
     # all xvalues are 0, a ValueError is raised when trying to set
     # xscale
     plt.xlim(xtick_values[0], xtick_values[-1])
+    plt.ylim(ytick_values[0], ytick_values[-1])
 
     plt.xscale("log")
     plt.xticks(xtick_values, map(str, xtick_values))
-    
-    plt.yticks(ytick, [ str(y * 100) for y in ytick_values ])
+
+    plt.yticks(ytick, ytick_labels)
 
     plt.legend(loc='upper left', borderaxespad=0, fontsize='small')
     plt.xlabel("Rate of false alarm (per minute)")
