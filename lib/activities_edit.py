c71176cbf81ded9340da840b0640532df14796bd
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2020-10-08 10:13:59 -0400

    Add Jesse's modifications

--- lib/activities_edit.py
+++ lib/activities_edit.py
@@ -1,5 +1,14 @@
 import json
 import argparse
+import sys
+import os
+import pandas as pd
+lib_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../../lib")
+sys.path.append(lib_path)
+from sparse_signal import SparseSignal as ss
+import metrics
+
 
 def boundingbox_merge(my_data, aggre_type):
     if aggre_type == 'activity':
@@ -42,6 +51,8 @@ def boundingbox_merge(my_data, aggre_type):
                 # Destroy stray objects
                 while len(my_data['activities'][i]['objects']) > 1:
                     my_data['activities'][i]['objects'].pop(1)
+
+        return my_data
     elif aggre_type == 'frame':
         for i in range(0, len(my_data['activities'])):
             loc_id = list(my_data['activities'][i]['localization'].keys())[0]
@@ -99,7 +110,56 @@ def boundingbox_merge(my_data, aggre_type):
                 # Destroy stray objects
                 while len(my_data['activities'][i]['objects']) > 1:
                     my_data['activities'][i]['objects'].pop(1)
-    return my_data
+
+        return my_data
+    elif aggre_type == 'unique':
+        my_df = pd.DataFrame(columns=['activity', 'activityID', 'clipID'])
+        my_data = my_json
+        my_key_dict = dict()
+        for i in range(0, len(my_data['activities'])):
+            video_key = list(my_data['activities'][i]['localization'].keys())[0]
+            norm_frames = list(my_data['activities'][i]['localization'][video_key])
+            norm_frames.sort()
+            norm_frames = [int(i) for i in norm_frames]
+            temporal_padding = min(150, max((norm_frames[1]-norm_frames[0]), 60))
+            active_frames = dict()
+            if norm_frames[0] - temporal_padding < 0:
+                active_frames['0'] = 1
+            else:
+                active_frames[str(norm_frames[0]-temporal_padding)] = 1
+            active_frames[str(norm_frames[1]+temporal_padding)] = 0
+            active_frames = ss(active_frames)
+            activity = my_data['activities'][i]['activity']
+            if activity in my_key_dict:
+                my_key_dict[activity] = my_key_dict[activity] + active_frames
+            else:
+                my_key_dict[activity] = active_frames
+        for j in range(0, len(my_data['activities'])):
+            video_key = list(my_data['activities'][j]['localization'].keys())[0]
+            norm_frames = list(my_data['activities'][j]['localization'][video_key])
+            norm_frames.sort()
+            norm_frames = [int(i) for i in norm_frames]
+            temporal_padding = min(150, max((norm_frames[1] - norm_frames[0]), 60))
+            active_frames = dict()
+            if norm_frames[0] - temporal_padding < 0:
+                active_frames['0'] = 1
+            else:
+                active_frames[str(norm_frames[0] - temporal_padding)] = 1
+            active_frames[str(norm_frames[1] + temporal_padding)] = 0
+            active_frames = list(active_frames)
+            activity = my_data['activities'][j]['activity']
+            activ_keys = list(my_key_dict[activity].keys())
+            activ_keys = [int(i) for i in activ_keys]
+            activ_keys.sort()
+            activ_bool_list = [int(active_frames[1]) > i > int(active_frames[0]) for i in activ_keys]
+            if all(flg is False for flg in activ_bool_list) and \
+                    my_key_dict[activity][active_frames[0]] == 1 and \
+                    my_key_dict[activity][active_frames[1]] == 0:
+                act_id = my_data['activities'][j]['activityID']
+                my_df = my_df.append({'activity': activity, 'activityID': act_id, 'clipID': video_key},
+                               ignore_index=True)
+        return my_df
+
 
 
 if __name__ == "__main__":
@@ -108,11 +168,17 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", action="store", dest="fout_path")
     parser.add_argument("-t", "--type", action="store", dest="aggregation_type")
     args = parser.parse_args()
+
     with open(args.fin_path) as f:
         my_json = json.load(f)
     aggre_type = args.aggregation_type
-    if aggre_type not in ['frame', 'activity']:
+    if aggre_type not in ['frame', 'activity', 'unique']:
         aggre_type = 'frame'
-    output_json = boundingbox_merge(my_json, aggre_type)
-    with open(args.fout_path, 'w') as json_file:
-        json.dump(output_json, json_file, indent=2, sort_keys=True)
+    if aggre_type in ['frame', 'activity']:
+        output_json = boundingbox_merge(my_json, aggre_type)
+        with open(args.fout_path, 'w') as json_file:
+            json.dump(output_json, json_file, indent=2, sort_keys=True)
+    if aggre_type in ['unique']:
+        output_csv = boundingbox_merge(my_json, aggre_type)
+        output_csv.to_csv(path_or_buf=args.fout_path)
+
