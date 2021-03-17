6a5ae8b61bda07da9f369a3411a416b2e5637f98
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2021-03-17 09:50:11 -0400

    remove unused script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 281,5
-time python3 ActEV_Scorer.py ActEV_SDL_V2  -FdmS -n 6 -v \
-    -o ../scorer_output \
-    -a ../system/activity-index.json \
-    -f ../system/file-index.json \
-    -r ../system/activities.json \
-    -s ../system/system-output.json
